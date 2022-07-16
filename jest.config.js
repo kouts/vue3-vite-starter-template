13bad7d905f7b5df720e0fc723319fae082b1f61
Kouts <koutsaftakis@gmail.com>
2022-07-16 10:05:34 +0300

    fix: updated npm packages and vite to v3

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.vue$': '@vue/vue3-jest'
-  }
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\js$': 'babel-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js)$',
+  moduleFileExtensions: ['vue', 'js'],
+  moduleNameMapper: {
+    // Fix in order for vue-test-utils to work with Jest 28
+    // https://github.com/vuejs/vue-test-utils/issues/1975
+    '^@vue/test-utils$': require.resolve('@vue/test-utils'),
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+  coverageReporters: ['text', 'json-summary']
 }
